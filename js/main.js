3e6ba96b174c63f203516f9b66e95ad6207f154a
Ashok Shah <you@example.com>
2020-01-31 15:12:56 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,76 @@
+
+// avigation
+$(window).on('scroll',function(){
+        if ($(window).scrollTop()){
+          $('nav').addClass('black');
+        }
+        else 
+        {
+            $('nav').removeClass('black');
+        }
+      });
+
+
+
+// dashboard
+$( function() {
+    $( ".toggle-nav" ).click( function() {
+      $( 'body' ).toggleClass( "sidebar-animate" );
+    });
+  });
+  
+  
+  $(document).ready(function(){
+  $('.launch-modal').click(function(){
+    $('#add-charges').modal({
+      backdrop: 'static'
+    });
+  }); 
+});
+
+$('.add-clon').on('click', '.remove', function() {
+  $('.remove').closest('.add-clon').find('.element').not(':first').last().remove();
+});
+$('.add-clon').on('click', '.clone', function() {
+  $('.clone').closest('.add-clon').find('.element').first().clone().appendTo('.results');
+})
+
+$(".dsy button").addClass("active");
+ 
+ $(document).ready(function(){
+  $('button').click(function(){
+    $('.dsy  button').removeClass("active");
+    $(this).addClass("active");
+});
+});
+
+
+
+ // calendar
+
+ $(document).ready(function () {
+         $("#from5").datepicker({
+          dateFormate:'dd/mm/yy',
+         });
+       });
+ $(document).ready(function () {
+         $("#to6").datepicker({
+          dateFormate:'dd/mm/yy',
+         });
+       });
+  $(document).ready(function () {
+         $("#from7").datepicker({
+          dateFormate:'dd/mm/yy',
+         });
+       });
+ $(document).ready(function () {
+         $("#to7").datepicker({
+          dateFormate:'dd/mm/yy',
+         });
+       });
+ 
+
+ // pophover
+ $(function () {
+  $('[data-toggle="popover"]').popover();
+});
